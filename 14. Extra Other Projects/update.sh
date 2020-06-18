1d72679e21f7c8acfb118bfe760deb9b7535ec0c
Dhruv Jain <33798277+Feetly@users.noreply.github.com>
2020-06-18 15:27:00 +0530

    Add files via upload

---
+++ 14. Extra Other Projects/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get -y update && sudo apt-get -y upgrade
+cd /var/www/
+sudo rm -r html
+sudo wget -q  https://github.com/DestroyerAlpha/MilestoneIndustries/archive/develop.zip
+sudo  unzip -o -q *.zip && sudo rm -f *.zip
+sudo mv -f MilestoneIndustries-develop/ html/
+cd ~
+
