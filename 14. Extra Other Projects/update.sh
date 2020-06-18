8d127c4f4786541e1e8a85451fe8c086c49cf04f
Dhruv Jain <33798277+Feetly@users.noreply.github.com>
2020-06-18 15:26:46 +0530

    Delete update.sh

--- 14. Extra Other Projects/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get -y update && sudo apt-get -y upgrade
-sudo clear
-cd /var/www/
-sudo rm -r html
-sudo wget -q  https://github.com/DestroyerAlpha/MilestoneIndustries/archive/develop.zip
-sudo  unzip -o -q *.zip && sudo rm -f *.zip
-sudo mv -f MilestoneIndustries-develop/ html/
