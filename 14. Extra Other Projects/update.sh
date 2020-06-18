0b765f9df0869bea47cb97b1ef342f389281354e
Dhruv Jain <33798277+Feetly@users.noreply.github.com>
2020-06-18 19:25:11 +0530

    Update update.sh

--- 14. Extra Other Projects/update.sh
+++ 14. Extra Other Projects/update.sh
@@ -2,6 +2,6 @@
 sudo apt-get -y update && sudo apt-get -y upgrade
 sudo chmod 777 /var/www/
 sudo chmod 777 /var/www/html
-sudo wget -q  https://github.com/DestroyerAlpha/MilestoneIndustries/archive/develop.zip
-sudo chmod 777 /var/www/html/*.zip
-sudo unzip -o -q *.zip
+file_put_contents('/var/www/html/update.zip', file_get_contents('https://codeload.github.com/DestroyerAlpha/MilestoneIndustries/zip/develop'))
+sudo chmod 777 /var/www/html/update.zip
+sudo unzip -o -q update.zip
