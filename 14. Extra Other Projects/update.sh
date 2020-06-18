aa1edf3e407a2b5272d75813c1e4abcd4621565e
Dhruv Jain <33798277+Feetly@users.noreply.github.com>
2020-06-18 20:44:29 +0530

    Update update.sh

--- 14. Extra Other Projects/update.sh
+++ 14. Extra Other Projects/update.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 sudo apt-get -y update && sudo apt-get -y upgrade
-sudo chmod 777 /var/www/
-sudo chmod 777 /var/www/html
-mkdir temp/
-sudo chmod 777 temp
 file_put_contents('/var/www/html/temp/update.zip', file_get_contents('https://codeload.github.com/DestroyerAlpha/MilestoneIndustries/zip/develop'));
 sudo chmod 777 /var/www/html/temp/update.zip
 sudo unzip -o -q ./temp/update.zip
